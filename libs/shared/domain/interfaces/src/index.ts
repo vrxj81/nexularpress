0b5df5068d4e43b0d686275e386ea7def58c40de
Johan Vrolix <johan@vrolix.eu>
2024-03-23 14:28:49 +0100

    fix(domain-interfaces) fixed WP Post interface to match other interfaces rendered fields. fixed List Post Query parameters naming

--- libs/shared/domain/interfaces/src/index.ts
+++ libs/shared/domain/interfaces/src/index.ts
@@ -1,4 +1,4 @@
-export * from './lib/arguments/post-query-params.interface';
+export * from './lib/arguments/list-post-query-params.interface';
 export * from './lib/arguments/retrieve-post-query-params.interface';
 export * from './lib/resources/post.interface';
 export * from './lib/arguments/list-categories-query-params.interface';
